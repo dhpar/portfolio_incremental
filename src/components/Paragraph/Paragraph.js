7cff5de60f70fa23e90e4eb47e144e1a1b68e1e7
David Parramon <david.parramon@Nerderys-MacBook-Pro.local>
2020-09-22 18:29:22 -0500

    fixing right space in right aligned h2 (with text-indent negative)

--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,7 +1,7 @@
 import React from "react";
 // Helper component that we need to use in order to have breaklines when in the json data file enter a \\n
-export default ({text}) => {
-    return '<p key={index} classname={classStyle}>' + text.split('\\n').join('<p key={index} classname={classStyle}>', '</p>') + '<p>';
-    // return text.split('\\n').map((paragraph, index) => 
-    //     <p key={index} classname={classStyle}>{paragraph}</p>)
-}
+export default ({text}) => 
+    text
+        .split('\\n')
+        .map((paragraph, index) => 
+            <p key={index}>{paragraph}</p>)
